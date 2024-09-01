7498ed48270e464560b39bc944740edf06c3019e
Mohit <mohitrai254@gmail.com>
2024-09-01 16:17:56 +0530

    designing

--- src/Password.jsx
+++ src/Password.jsx
@@ -31,29 +31,38 @@ function Password() {
   };
 
   return (
-    <div className="h-screen w-screen justify-center items-center bg-green-100 flex flex-col">
-      <div className="text-3xl font-sans bg-[#49B6FF] rounded w-1/2 h-20">
-        <h1 className="text-center text-[#FF5E5B]">Password Generator</h1>
+    <div
+      style={{ fontFamily: "Georgia, serif" }}
+      className="h-screen w-screen justify-center items-center flex flex-col gap-4"
+    >
+      <div className="text-3xl font-sans bg-[#fe675c] bg-opacity-[0.9] rounded-3xl w-1/2 h-20 p-4 border-indigo-500">
+        <h1 className="text-center text-slate-200">PASSWORD GENERATOR</h1>
       </div>
-      <div className="border-2 rounded w-1/2 h-80 flex flex-col">
-        <div className="flex flex-col">
-          <div>
-            <label>Generate Password</label>
+      <div className="border-2 rounded-3xl w-1/2 h-60 flex flex-col p-4 bg-slate-200 bg-opacity-[0.8] border-red-300 shadow-2xl justify-center items-center">
+        <div className="flex flex-col gap-8 font-serif ">
+          <div className="space-x-4">
+            <label>Generated Password :</label>
             <input
-              className="border-2"
+              className="border-2 rounded-lg p-1 border-red-300 "
               type="text"
               value={password}
               ref={inputRef}
               readOnly
             />
-            <button onClick={handleCopy}>Copy</button>
+            <button
+              className="border-2 rounded-3xl w-[55px] p-1 border-red-300 hover:bg-blue-500 bg-[#fe675c] text-slate-200"
+              onClick={handleCopy}
+            >
+              Copy
+            </button>
           </div>
 
-          <div>
-            <label>
-              Length:{length}
+          <div className="flex gap-4">
+            <label className="">
+              Length: {length}{" "}
               <input
                 type="range"
+                value={length}
                 onChange={(e) => setLength(e.target.value)}
                 min={8}
                 max={20}
